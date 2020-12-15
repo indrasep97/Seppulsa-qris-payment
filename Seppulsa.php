d8756c07b6c22f6e4eb705679666ff54a9f83dcc
indra septian <43588813+indrasep97@users.noreply.github.com>
2020-12-15 14:24:24 +0700

    Update Seppulsa.php

--- Seppulsa.php
+++ Seppulsa.php
@@ -1,20 +1,21 @@
 <?php
 class Seppulsa
 {
-function qris($nama,$email,$reff,$nominal,$callback){
+function qris($nama,$email,$reff,$nominal,$return,$callback){
 $api = ""; //isi api payment yang ada di menu api
 $url = "https://seppulsa.com/payment"; 
 $contents = [
 "merchant"=>$api,
 "nominal"=>$nominal,
-""=>"",
-""=>"",
-""=>"",
-""=>"",
-""=>"",
-""=>"",
-
+"nama"=>$nama,
+"email"=>$email,
+"reff"=>$reff,
+"return"=>$return,
+"callback"=>$callback,
+"format"=>"json",
 ];
+$data = http_build_query($contents);
+
 $ch = curl_init(); 
 curl_setopt($ch, CURLOPT_URL, $url); 
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
