c12643ddd418b3046544fa4c5e77e290c6e5b7e3
indra septian <43588813+indrasep97@users.noreply.github.com>
2020-12-15 14:07:52 +0700

    Update Seppulsa.php

--- Seppulsa.php
+++ Seppulsa.php
@@ -1,3 +1,16 @@
 <?php
+class Seppulsa
+{
+function qris($nama,$email,$reff,$nominal){
+$api = ""; //isi api payment yang ada di menu api
+$url = "https://seppulsa.com/payment"; 
 
-?>
+$ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $url); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$output = curl_exec($ch); 
+curl_close($ch); 
+return $output;
+}
+}
