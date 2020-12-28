43b691ed5e8000e574946c736ae63e8699a62307
indra septian <43588813+indrasep97@users.noreply.github.com>
2020-12-28 08:49:54 +0700

    Update Seppulsa.php

--- Seppulsa.php
+++ Seppulsa.php
@@ -22,7 +22,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-output = curl_exec($ch); 
+$output = curl_exec($ch); 
 curl_close($ch); 
 return $output;
 }
