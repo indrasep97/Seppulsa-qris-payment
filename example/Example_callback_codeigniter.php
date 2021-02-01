0f1450b9fbcc0323579b7a1c15f420b67dd44f3c
indra septian <43588813+indrasep97@users.noreply.github.com>
2021-02-01 20:57:54 +0700

    Update Example_callback_codeigniter.php

--- example/Example_callback_codeigniter.php
+++ example/Example_callback_codeigniter.php
@@ -1,5 +1,25 @@
 <?php 
 Class callback extends CI_Controller{
 
+Public function index(){
+$id = $_POST['id'];
+$status = $_POST['status'];
+$reff = $_POST['reff'];
+$verif = $_POST['verif'];
+
+$key = ""; //isi dengan key verif yang di input di menu api
+
+if(isset($_POST['id'])){
+ if($verif != $key ){
+   die('KEY TIDAK FAILED');
+     }else{
+      if($status == "sukses"){
+        //proses sistem anda jika pembayaran sukses
+        }else{ 
+         //proses sistem  anda jika pembayaran gagal
+        }
+      }
+   }
+ }
 
 }
